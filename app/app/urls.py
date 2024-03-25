f8d5c38e7cd2688f766a8a4e06507e5d51a2ca46
pugalo0123 <nicglad2005@gmail.com>
2024-03-25 14:21:11 +0700

    Register simple tag for categories

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
-from app.settings import DEBUG
 
 
 urlpatterns = [
@@ -27,7 +28,9 @@ urlpatterns = [
 
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
+
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
